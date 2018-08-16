7dc4f1795bbd7adb833446d0a32b6551d0c9e8f0
Pawel Bachta <bachta.pawel@gmail.com>
2018-08-16 10:48:58 +0200

    play with jpa and hibernate annotations

--- jpaHibernate/src/main/java/sandbox/jpaHibernate/jpaHibernate/entity/Course.java
+++ jpaHibernate/src/main/java/sandbox/jpaHibernate/jpaHibernate/entity/Course.java
@@ -1,17 +1,31 @@
 package sandbox.jpaHibernate.jpaHibernate.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 
 @Entity
+@NamedQuery(name="get_all_courses", query =  "Select c From Courses c")
 public class Course {
 
     @Id
     @GeneratedValue
     private Long id;
 
+    @Column(nullable = false)
     private String name;
+
+    @UpdateTimestamp
+    private LocalDateTime lastUpdatedDate;
+    @CreationTimestamp
+    private LocalDateTime createdDate;
 
     public Course() {
     }
--- jpaHibernate/src/main/resources/data.sql
+++ jpaHibernate/src/main/resources/data.sql
@@ -1,4 +1,12 @@
+/*
 INSERT INTO COURSE(ID, NAME) VALUES(10001, 'Some course no 1 name');
 INSERT INTO COURSE(ID, NAME) VALUES(10002, 'Some course no 2 name');
 INSERT INTO COURSE(ID, NAME) VALUES(10003, 'Some course no 3 name');
 INSERT INTO COURSE(ID, NAME) VALUES(10004, 'Some course no 4 name');
+*/
+INSERT INTO COURSE(ID, NAME, created_date, last_updated_date) VALUES(10001, 'Some course no 1 name', sysdate(), sysdate());
+INSERT INTO COURSE(ID, NAME, created_date, last_updated_date) VALUES(10002, 'Some course no 2 name', sysdate(), sysdate());
+INSERT INTO COURSE(ID, NAME, created_date, last_updated_date) VALUES(10003, 'Some course no 3 name', sysdate(), sysdate());
+INSERT INTO COURSE(ID, NAME, created_date, last_updated_date) VALUES(10004, 'Some course no 4 name', sysdate(), sysdate());
+
+
